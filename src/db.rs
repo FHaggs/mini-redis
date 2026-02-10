94ca34c5d245039c0f0af96ee9c35cd382458936
FHaggs <pedrofamh@protonmail.com>
2026-02-10 15:23:02 +0100

    Remove channel indirection

--- src/db.rs
+++ src/db.rs
@@ -6,30 +6,26 @@ use tokio::sync::mpsc;
 pub type Db = HashMap<Bytes, Bytes>;
 use crate::protocol::Response;
 
-use tokio::sync::oneshot::Sender;
-
 #[derive(Debug)]
 pub enum DbCommand {
-    Set { key: Bytes, value: Bytes, req_id:  u32, tx: Sender<Response> },
-    Get { key: Bytes, tx: Sender<Response>, req_id:  u32 },
+    Set { key: Bytes, value: Bytes, req_id: u32, resp_tx: mpsc::UnboundedSender<Response> },
+    Get { key: Bytes, req_id: u32, resp_tx: mpsc::UnboundedSender<Response> },
 }
 
 pub async fn db_manager(mut db_rx: mpsc::UnboundedReceiver<DbCommand>, mut db: Db) {
     println!("DB manager started");
     while let Some(command) = db_rx.recv().await {
         match command {
-            DbCommand::Get { key, tx, req_id } => {
-                //print!("DB GET: {:?} => ", key);
+            DbCommand::Get { key, req_id, resp_tx } => {
                 let res = db.get(&key).map(|v| v.clone());
                 let _ = match res {
-                    Some(value) => tx.send(Response::Ok { req_id, value: Some(value) }),
-                    None => tx.send(Response::NotFound { req_id }),
+                    Some(value) => resp_tx.send(Response::Ok { req_id, value: Some(value) }),
+                    None => resp_tx.send(Response::NotFound { req_id }),
                 };
             }
-            DbCommand::Set { key, value, req_id, tx } => {
-                //print!("DB SET: {:?} => {:?}\n", key, value);
+            DbCommand::Set { key, value, req_id, resp_tx } => {
                 db.insert(key, value);
-                let _ = tx.send(Response::Ok { req_id, value: None });
+                let _ = resp_tx.send(Response::Ok { req_id, value: None });
             }
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ use bytes::BytesMut;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::net::tcp::OwnedReadHalf;
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::mpsc;
 
 pub async fn process(
     socket: TcpStream,
@@ -14,8 +14,8 @@ pub async fn process(
 ) -> Result<()> {
     let (read_half, write_half) = socket.into_split();
 
-    // Channel to send response receivers from reader to writer
-    let (resp_tx, resp_rx) = mpsc::unbounded_channel::<oneshot::Receiver<Response>>();
+    // Single response channel for this connection - no per-request allocations
+    let (resp_tx, resp_rx) = mpsc::unbounded_channel::<Response>();
 
     // Spawn writer task
     let writer_handle = tokio::spawn(writer_task(write_half, resp_rx));
@@ -35,7 +35,7 @@ pub async fn process(
 async fn reader_task(
     mut socket: OwnedReadHalf,
     db_channel: mpsc::UnboundedSender<DbCommand>,
-    sender_to_writer: mpsc::UnboundedSender<oneshot::Receiver<Response>>,
+    resp_tx: mpsc::UnboundedSender<Response>,
 ) -> Result<()> {
     let mut frame = Frame::new();
     let mut buf = BytesMut::with_capacity(MAX_FRAME);
@@ -49,21 +49,22 @@ async fn reader_task(
             }
         }
         while let Some(command) = frame.try_parse(&mut buf) {
-            let (tx, rx) = oneshot::channel();
+            // Clone is cheap - just Arc increment
             let cmd = match command {
-                Command::Get { req_id, key } => DbCommand::Get { key, tx, req_id },
+                Command::Get { req_id, key } => DbCommand::Get {
+                    key,
+                    req_id,
+                    resp_tx: resp_tx.clone(),
+                },
                 Command::Set { req_id, key, value } => DbCommand::Set {
                     key,
                     value,
                     req_id,
-                    tx,
+                    resp_tx: resp_tx.clone(),
                 },
             };
             if db_channel.send(cmd).is_err() {
                 return Err("db manager dropped".into());
-            }
-            if sender_to_writer.send(rx).is_err() {
-                return Err("writer dropped".into());
             }
         }
     }
@@ -72,31 +73,23 @@ async fn reader_task(
 
 async fn writer_task(
     mut write_half: tokio::net::tcp::OwnedWriteHalf,
-    mut resp_rx: mpsc::UnboundedReceiver<oneshot::Receiver<Response>>,
+    mut resp_rx: mpsc::UnboundedReceiver<Response>,
 ) -> Result<()> {
     let mut response_buf = BytesMut::with_capacity(4096);
 
-    while let Some(rx) = resp_rx.recv().await {
-        match rx.await {
-            Ok(res) => {
-                response_buf.extend_from_slice(&encode_response(res));
+    while let Some(res) = resp_rx.recv().await {
+        response_buf.extend_from_slice(&encode_response(res));
 
-                // Drain any other ready responses (batching)
-                while let Ok(rx) = resp_rx.try_recv() {
-                    match rx.await {
-                        Ok(res) => response_buf.extend_from_slice(&encode_response(res)),
-                        Err(_) => return Err("db response dropped".into()),
-                    }
-                }
-
-                // Write batch
-                if let Err(err) = write_half.write_all(&response_buf).await {
-                    return Err(err.into());
-                }
-                response_buf.clear();
-            }
-            Err(_) => return Err("db response dropped".into()),
+        // Drain any other ready responses (batching)
+        while let Ok(res) = resp_rx.try_recv() {
+            response_buf.extend_from_slice(&encode_response(res));
         }
+
+        // Write batch
+        if let Err(err) = write_half.write_all(&response_buf).await {
+            return Err(err.into());
+        }
+        response_buf.clear();
     }
 
     Ok(())
